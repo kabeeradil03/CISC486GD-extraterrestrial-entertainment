d17330d90d8e70210859ae1db4b296b220c71e43
Nicholas Tillo <nicholastillo@gmail.com>
2025-10-02 10:40:46 -0400

    Added some more jounral & Player FSM stuff, Also downloaded the TextMeshPro library, for text

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,6 +4,15 @@ public class GameController : MonoBehaviour
 {
     public int score;
     
+    public GameObject journalPrefab;
+    public GameObject jounralInstance;
+    public bool isJounralUIOpen;
+    public Camera jounralCamera; 
+    public Camera mainCamera;
+
+    
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +23,21 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnOpenJournal(){
+        if(!isJounralUIOpen){
+            jounralInstance = GameObject.Instantiate(journalPrefab);
+            jounralCamera.enabled = true;
+            mainCamera.enabled = false; 
+            isJounralUIOpen = true;
+
+        }
+        else{
+            Destroy(jounralInstance);
+            jounralCamera.enabled = false;
+            mainCamera.enabled = true;
+            isJounralUIOpen = false;
+
+        }
     }
 }
--- Assets/PlayerFSM.cs
+++ Assets/PlayerFSM.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerFSM : MonoBehaviour
 {
+    //Possible Player States. 
     public enum State{
         DecidingJoke,
         SayingJoke, 
@@ -9,16 +10,46 @@ public class PlayerFSM : MonoBehaviour
         Paused
     }
     public State currentState; 
+    public float waitingTime;
+
+    //UI Elements.
+    public GameObject jokePrompt; 
+    public GameObject jokeResponseContainer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentState = State.Waiting;
+        waitingTime = 5f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        waitingTime -= Time.deltaTime;
+        if(waitingTime<=0){
+            //Do Next Action, Call one of the transitions, depending on what conditions are true.
+        }
         
+    }
+
+    void randJoke(){
+
+    }
+    //Transition between states; 
+    void WaitingToDeciding(){
+
+        //Generate Random Joke Prompt. 
+        GameObject jokeInstance = GameObject.Instantiate(jokePrompt);
+        //jokeResponseContainer.enabled = true;
+        
+        
+    }
+    void DecidingToSaying(){
+        
+    }
+    void SayingToWaiting(){
+
     }
 }
