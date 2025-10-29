b5e4010658cdc2f81dedaf9aa19af1f060aff3e2
Marcus9986 <122645673+Marcus9986@users.noreply.github.com>
2025-10-28 20:44:27 -0400

    Adding Main Menu and fixes

--- Assets/NicholasWork/AlienMovement.cs
+++ Assets/NicholasWork/AlienMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class AlienMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int movementSpeed = 5f; 
+    public float movementSpeed = 5f; 
     
     void Start()
     {
