32e972a0015e611c60e3e159327bf2e8e8ad558b
Marcus9986 <122645673+Marcus9986@users.noreply.github.com>
2025-11-21 19:36:31 -0500

    Audio Changes
    made the main menu and ambient music loop. made it so the bar audio is not as loud when preparing a joke

---
+++ Assets/MarcusWork/Scripts/AudioLevel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AudioLevel : MonoBehaviour
+{
+
+    public AudioSource aud;
+    public PlayerFSM pfsm;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pfsm.currentState == PlayerFSM.State.DecidingJoke)
+        {
+            aud.volume = 0.1f;
+        } else
+        {
+            aud.volume = 1;
+        }
+    }
+}
