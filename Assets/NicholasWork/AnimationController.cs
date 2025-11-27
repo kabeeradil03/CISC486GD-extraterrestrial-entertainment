b385de7e8021580de7636fdaba4b9df28d72ffd2
Nicholas Tillo <nicholastillo@gmail.com>
2025-11-27 14:21:21 -0500

    Update AnimationController.cs

--- Assets/NicholasWork/AnimationController.cs
+++ Assets/NicholasWork/AnimationController.cs
@@ -16,7 +16,7 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ship.AddForce(new Vector3(0, 60f, 0));
+        ship.AddForce(new Vector3(0, 60f, 0) * Time.deltaTime);
 
         if(endingDelayTimer > 10f)
         {
