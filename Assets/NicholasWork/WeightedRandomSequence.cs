3d3e9796bc420affc11952d84cfbdb27381dbb52
Nicholas Tillo <nicholastillo@gmail.com>
2025-11-27 14:11:13 -0500

    Update WeightedRandomSequence.cs

--- Assets/NicholasWork/WeightedRandomSequence.cs
+++ Assets/NicholasWork/WeightedRandomSequence.cs
@@ -3,6 +3,7 @@ using Unity.Behavior;
 using UnityEngine;
 using Composite = Unity.Behavior.Composite;
 using Unity.Properties;
+using System.Diagnostics;
 
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "WeightedRandom", story: "Weighted Random Based On State", category: "Flow", id: "cd12de49a13a777d896b62e5f063e845")]
@@ -26,7 +27,7 @@ public partial class WeightedRandomSequence : Composite
         //Navigate To Bar, Navigate To Listening Chair, Navigate To Other Chair, Wait, Wander, Leave, 
         float[] arrayVeryHappy = { 10, 70, 80, 85, 95, 100 };
         float[] array1Happy = { 20, 60, 75, 80, 90, 100 };
-        float[] array2Neutral = { 20, 75, 80,85, 95, 100 };
+        float[] array2Neutral = { 20, 30, 80, 85, 95, 100 };
         float[] array3Angry = { 30, 50, 60, 65, 70, 100 };
         float[] array4VeryAngry = { 30, 40, 50, 55, 60, 100 };
         float[] array5Sad = { 20, 30, 40,45, 50, 100 };
@@ -59,19 +60,17 @@ public partial class WeightedRandomSequence : Composite
         else
         {
             correspondingWeights = array5Sad;
-            
+
         }
 
-
-
+        
         m_RandomNumber = UnityEngine.Random.Range(0, 100); // Turn this into a weigthed range.
         int chosenIndex = 0;
         while (m_RandomNumber > correspondingWeights[chosenIndex])
         {
             chosenIndex += 1;
         }
-
-
+        UnityEngine.Debug.Log(chosenIndex);
         //Execute the corresponding State 
         if (chosenIndex < Children.Count)
         {
